cd150e526832fc604b0b17e93982817c3fa0d251
SimunSpoljaric <sspoljari@pmfst.hr>
2024-02-26 15:55:03 +0100

    commit

---
+++ Zadatak5.py
@@ -0,0 +1,5 @@
+"""Zadatak 5
+Unaprijedite kod iz prethodnog zadatka koristeci modul matplotlib.pyplot tako da nacrtate unesene koordinate i pravac koji prolazi kroz njih. 
+Ponudite u funkciji opciju da se graf prikaze na ekranu ili da se spremi
+kao PDF. Dopustite korisniku da bira ime pod kojim ce se spremiti graf.
+"""
