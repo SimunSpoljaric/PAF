6b038145db35e366bd405af37a106ba51c35ba98
Student <sspoljari@pmfst.hr>
2024-02-26 15:48:18 +0100

    commit

---
+++ Vjezba1/Zadatak4.py
@@ -0,0 +1,4 @@
+"""Zadatak 4
+Napisite funkciju koja kao ulazne parametre prima (x, y) koordinate za dvije tocke. Neka ta funkcija na
+ekran ispisuje jednadzbu pravca koji prolazi kroz te dvije tocke. Pozovite tu funkciju u svom programu.
+"""
